5e67156817de1d68edddd0049a5c002343c7961d
Yifeng Chen <virgilchen97@gmail.com>
2019-11-26 14:24:58 -0600

    eng problem name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 """
 这是一个将力扣中国(leetcode-cn.com)上的【个人提交】的submission自动爬到本地并push到github上的爬虫脚本。
 请使用相同目录下的config.json设置 用户名，密码，本地储存目录等参数。
@@ -6,13 +6,17 @@
 """
 
 import unicodedata
-import sys, os, time
+import sys
+import os
+import time
 from ProblemList import GetProblemId
-import requests, json
+import requests
+import json
 from bs4 import BeautifulSoup
 import json
-#~~~~~~~~~~~~以下是无需修改的参数~~~~~~~~~~~~~~~~·
-requests.packages.urllib3.disable_warnings() #为了避免弹出一万个warning，which is caused by 非验证的get请求
+# ~~~~~~~~~~~~以下是无需修改的参数~~~~~~~~~~~~~~~~·
+# 为了避免弹出一万个warning，which is caused by 非验证的get请求
+requests.packages.urllib3.disable_warnings()
 
 leetcode_url = 'https://leetcode-cn.com/'
 
@@ -20,42 +24,42 @@ sign_in_url = 'accounts/login/'
 sign_in_url = leetcode_url + sign_in_url
 submissions_url = 'submissions/'
 submissions_url = leetcode_url + submissions_url
+dic = {}
 
-with open("config.json", "r") as f: #读取用户名，密码，本地存储目录
-    temp = json.loads(f.read())   
+with open("config.json", "r") as f:  # 读取用户名，密码，本地存储目录
+    temp = json.loads(f.read())
     USERNAME = temp['username']
     PASSWORD = temp['password']
     OUTPUT_DIR = temp['outputDir']
     TIME_CONTROL = 3600 * 24 * temp['time']
-#~~~~~~~~~~~~以上是无需修改的参数~~~~~~~~~~~~~~~~·
+# ~~~~~~~~~~~~以上是无需修改的参数~~~~~~~~~~~~~~~~·
 
-#~~~~~~~~~~~~以下是可以修改的参数~~~~~~~~~~~~~~~~·
+# ~~~~~~~~~~~~以下是可以修改的参数~~~~~~~~~~~~~~~~·
 START_PAGE = 0  # 从哪一页submission开始爬起，0是最新的一页
 sleep_time = 5  # in second，登录失败时的休眠时间
-#~~~~~~~~~~~~以上是可以修改的参数~~~~~~~~~~~~~~~~·
+# ~~~~~~~~~~~~以上是可以修改的参数~~~~~~~~~~~~~~~~·
 
-def login(email, password): # 本函数copy自https://gist.github.com/fyears/487fc702ba814f0da367a17a2379e8ba，感谢@fyears
+
+def login(email, password):  # 本函数copy自https://gist.github.com/fyears/487fc702ba814f0da367a17a2379e8ba，感谢@fyears
     client = requests.session()
     client.encoding = "utf-8"
 
     while True:
         try:
             client.get(sign_in_url, verify=False)
-
-            login_data = {'login': email, 
-                'password': password
-            }
-            
+            login_data = {'login': email, 'password': password}
             result = client.post(sign_in_url, data=login_data, headers=dict(Referer=sign_in_url))
-            
+
             if result.ok:
-                print ("Login successfully!")
+                print("Login successfully!")
                 break
+
         except:
-            print ("Login failed! Wait till next round!")
+            print("Login failed! Wait till next round!")
             time.sleep(sleep_time)
 
     return client
+
 
 def scraping(client):
     page_num = START_PAGE
@@ -65,82 +69,101 @@ def scraping(client):
                    "c": ".c", "javascript": ".js", "php": ".php", "csharp": ".cs", "ruby": ".rb", "swift": ".swift",
                    "scala": ".scl", "kotlin": ".kt", "rust": ".rs"}
 
-    submissions_url = "https://leetcode-cn.com/api/submissions/?offset=" + str(page_num) + "&limit=20&lastkey="
-    
+    page_num == START_PAGE
     while True:
-        print ("Now for page:", str(page_num))
-        if page_num == START_PAGE:
-            h = client.get(submissions_url, verify=False)
-        else:
-            submissions_url = submissions_url.replace("offset=" + str(page_num -1), "offset=" + str(page_num))
-            h = client.get(submissions_url, verify=False)
+        submissions_url = "https://leetcode-cn.com/api/submissions/?offset=" + str(page_num) + "&limit=20&lastkey="
+        print("Now url: ", str(submissions_url))
 
+        h = client.get(submissions_url, verify=False)
         t = time.time()
         invalidset = set()
         html = json.loads(h.text)
         if "submissions_dump" not in html:
-            print ("Warning! No previous submission is detected, please make sure you are logging in the correct account AND you once submitted codes on leetcode-cn.com")
-            break
+            if html["detail"] == '您没有执行该操作的权限。':
+                time.sleep(5)
+                continue
+            else:
+                print("No further submissions")
+                break
+
         for idx, submission in enumerate((html["submissions_dump"])):
             Status = submission['status_display']
-            Title = submission['title'].replace(" ","")
+            Title = submission['title'].replace(" ", "")
             Lang = submission['lang']
 
             if Status != "Accepted":
-                print (Title + " is not Accepted, continue for the next submission")
+                print(str(GetProblemId(Title)) + " is not Accepted")
                 continue
 
-            if t - submission['timestamp'] > TIME_CONTROL: #时间管理，本行代表只记录最近的TIME_CONTROL天内的提交记录
+            # 时间管理，本行代表只记录最近的TIME_CONTROL天内的提交记录
+            if t - submission['timestamp'] > TIME_CONTROL:
                 return
 
             try:
                 Pid = GetProblemId(Title)
-                
+
                 if Pid == 0 or Title in invalidset:
-                    print (Title + "Failed ! Due to unknown Pid! ")
-                    if Title not in invalidset: #第一次没找到
+                    print(str(GetProblemId(Title)) + " Failed ! Due to unknown Pid! ")
+                    if Title not in invalidset:  # 第一次没找到
                         with open("Log.txt", "a") as log:
                             log.write("Unknown PID happened for ", Title)
-
                         invalidset.add(Title)
 
                 else:
                     if visited[Pid] != 1:
-                        newpath = OUTPUT_DIR + "./" + '{:0=4}'.format(Pid) + "." + Title #存放的文件夹名
+                        newpath = OUTPUT_DIR + "/" + \
+                            '{:0=4}'.format(Pid) + "." + \
+                            str(dic.get(Pid))  # 存放的文件夹名
                         if not os.path.exists(newpath):
                             os.mkdir(newpath)
 
-                        filename = '{:0=4}'.format(Pid) + "-" + Title + file_format[Lang] #存放的文件名
-                        totalpath = os.path.join(newpath, filename) #把文件夹和文件组合成新的地址
+                        filename = '{:0=4}'.format(
+                            Pid) + "-" + str(dic.get(Pid)) + file_format[Lang]  # 存放的文件名
+                        totalpath = os.path.join(
+                            newpath, filename)  # 把文件夹和文件组合成新的地址
 
                         if os.path.exists(totalpath):
-                            print (newpath + "exists! Continue for the next submission!") #跳过本地已记录的submission
+                            # 跳过本地已记录的submission
+                            print(newpath + "exists! Continue for the next submission!")
                             continue
 
-                        with open(totalpath, "w") as f: #开始写到本地
+                        with open(totalpath, "w") as f:  # 开始写到本地
                             # print ("Writing begins!", totalpath)
                             f.write(submission['code'])
-                            print ("Writing ends!", totalpath)
-                            visited[Pid] = 1 #保障每道题只记录最新的AC解
-            except:
-                print("Unknown bug happned!")
-            
-        page_num += 1
+                            print(str(GetProblemId(Title)) + " Writing ends!")
+                            visited[Pid] = 1  # 保障每道题只记录最新的AC解
+            except Exception as e:
+                print(e.with_traceback)
+
+        page_num += 20
+
+
+def loadEngName():
+    with open("problem.json", "r", encoding='UTF-8') as f:
+        problems = json.load(f)["stat_status_pairs"]
+        for problem in problems:
+            num = problem["stat"]["question_id"]
+            title = problem["stat"]["question__title_slug"]
+            dic[num] = str(title)
+
 
 def git_push():
-    today = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+    today = time.strftime('%Y-%m-%d', time.localtime(time.time()))
     os.chdir(OUTPUT_DIR)
-    print (os.getcwd())
-    instructions = ["git add .","git status", "git commit -m \""+ today, "git push -u origin master"]
+    print(os.getcwd())
+    instructions = ["git add .", "git status",
+                    "git commit -m \"" + today, "git push -u origin master"]
     for ins in instructions:
         os.system(ins)
         # print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print ("~~~~~~~~~~~~~" + ins + " finished! ~~~~~~~~")
+        print("~~~~~~~~~~~~~" + ins + " finished! ~~~~~~~~")
         # print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 
 def main():
     email = USERNAME
     password = PASSWORD
+    loadEngName()
 
     print('login')
     client = login(email, password)
@@ -151,6 +174,7 @@ def main():
 
     git_push()
     print('Git PUSH finished')
+
 
 if __name__ == '__main__':
     main()
